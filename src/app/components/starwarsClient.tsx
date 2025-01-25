f93f794790b2584f1b2a6f0dcba774ee329d3749
Leon-azerty <maxime.noel@epitech.eu>
2025-01-25 20:46:10 +0100

    feat: add skeleton for pagination

--- src/app/components/starwarsClient.tsx
+++ src/app/components/starwarsClient.tsx
@@ -2,6 +2,15 @@
 
 import { ItemProps } from '@/lib/type'
 import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/ui/pagination'
+import {
   Table,
   TableBody,
   TableCell,
@@ -23,30 +32,48 @@ export default function StarwarsClient({ items }: { items: ItemProps }) {
         setSelected={setSelected}
         setSearchResult={setSearchResult}
       />
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {searchResult.length > 0
-            ? searchResult.map((item, index) => (
-                <TableRow key={index}>
-                  <TableCell className="font-medium">
-                    {item.name ? item.name : item.title}
-                  </TableCell>
-                </TableRow>
-              ))
-            : items[selected].map((item, index) => (
-                <TableRow key={index}>
-                  <TableCell className="font-medium">
-                    {item.name ? item.name : item.title}
-                  </TableCell>
-                </TableRow>
-              ))}
-        </TableBody>
-      </Table>
+      <div className="flex flex-col">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {searchResult.length > 0
+              ? searchResult.map((item, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="font-medium">
+                      {item.name ? item.name : item.title}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : items[selected].map((item, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="font-medium">
+                      {item.name ? item.name : item.title}
+                    </TableCell>
+                  </TableRow>
+                ))}
+          </TableBody>
+        </Table>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </section>
   )
 }
